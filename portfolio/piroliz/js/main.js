3b556bae039aaff66d815d8c4316850d109cf0c4
Isidas <99565745+Isidas@users.noreply.github.com>
2022-12-03 20:01:46 +0300

    Add files via upload

--- portfolio/piroliz/js/main.js
+++ portfolio/piroliz/js/main.js
@@ -67,63 +67,295 @@ setTimeout(() => {
   pirolizSvg.classList.add('hide');
 }, 2000);
 const aboutWrapper = document.querySelector('.about');
+const animateBlockAbout = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.about__block--one');
+    let el2 = document.querySelector('.about__block--two');
+    let el3 = document.querySelector('.about__block--fhree');
+    let el4 = document.querySelector('.about__block--four');
+    setTimeout(() => {
+      el1.classList.add('animate__zoomIn');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__zoomIn');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__zoomIn');
+      el3.classList.add('block-visible');
+    }, 1000);
+    setTimeout(() => {
+      el4.classList.add('animate__zoomIn');
+      el4.classList.add('block-visible');
+    }, 1500);
+  });
+};
+animateBlockAbout(aboutWrapper);
 const stagesWrapper = document.querySelector('.stages');
-const recastWrapper = document.querySelector('.recast');
+const animateBlockStages = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.stages__item--first');
+    let el2 = document.querySelector('.stages__item--second');
+    let el3 = document.querySelector('.stages__item--three');
+    setTimeout(() => {
+      el1.classList.add('animate__zoomIn');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__zoomIn');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__zoomIn');
+      el3.classList.add('block-visible');
+    }, 1000);
+  });
+};
+animateBlockStages(stagesWrapper);
+const investmentsWrapper = document.querySelector('.investments');
+const animateBlockInvestments = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.investments__item--first');
+    let el2 = document.querySelector('.investments__item--second');
+    let el3 = document.querySelector('.investments__item--three');
+    setTimeout(() => {
+      el1.classList.add('animate__zoomIn');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__zoomIn');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__zoomIn');
+      el3.classList.add('block-visible');
+    }, 1000);
+  });
+};
+animateBlockInvestments(investmentsWrapper);
+const projectsWrapper = document.querySelector('.project');
+const animateBlockProject = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.project__item--first');
+    let el2 = document.querySelector('.project__item--second');
+    let el3 = document.querySelector('.project__item--three');
+    let el4 = document.querySelector('.project__item--four');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__fadeInLeft');
+      el3.classList.add('block-visible');
+    }, 1000);
+    setTimeout(() => {
+      el4.classList.add('animate__animated');
+      el4.classList.add('animate__fadeInRight');
+      el4.classList.add('block-visible');
+    }, 1500);
+  });
+};
+animateBlockProject(projectsWrapper);
+const processWrapper = document.querySelector('.process');
+const animateBlockProcess = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.process__inner--first');
+    let el2 = document.querySelector('.process__inner--second');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+  });
+};
+animateBlockProcess(processWrapper);
+const productsWrapper = document.querySelector('.products');
+const animateBlockProducts = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.products__item--first');
+    let el2 = document.querySelector('.products__item--second');
+    let el3 = document.querySelector('.products__item--three');
+    let el4 = document.querySelector('.products__item--four');
+    let el5 = document.querySelector('.products__item--five');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInTopLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__fadeInLeft');
+      el3.classList.add('block-visible');
+    }, 1000);
+    setTimeout(() => {
+      el4.classList.add('animate__animated');
+      el4.classList.add('animate__fadeInBottomLeft');
+      el4.classList.add('block-visible');
+    }, 1500);
+    setTimeout(() => {
+      el5.classList.add('animate__animated');
+      el5.classList.add('animate__fadeInDown');
+      el5.classList.add('block-visible');
+    }, 2000);
+  });
+};
+animateBlockProducts(productsWrapper);
+const plusesWrapper = document.querySelector('.pluses');
+const animateBlockPluses = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.pluses__item--first');
+    let el2 = document.querySelector('.pluses__item--second');
+    let el3 = document.querySelector('.pluses__item--three');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__zoomIn');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__zoomIn');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__zoomIn');
+      el3.classList.add('block-visible');
+    }, 1000);
+  });
+};
+animateBlockPluses(plusesWrapper);
+const vantageWrapper = document.querySelector('.vantage');
+const animateBlockVantage = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.vantage__item--first');
+    let el2 = document.querySelector('.vantage__item--second');
+    let el3 = document.querySelector('.vantage__item--three');
+    let el4 = document.querySelector('.vantage__item--four');
+    let el5 = document.querySelector('.vantage__item--five');
+    let el6 = document.querySelector('.vantage__item--six');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInTopLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__fadeInLeft');
+      el3.classList.add('block-visible');
+    }, 1000);
+    setTimeout(() => {
+      el4.classList.add('animate__animated');
+      el4.classList.add('animate__fadeInBottomLeft');
+      el4.classList.add('block-visible');
+    }, 1500);
+    setTimeout(() => {
+      el5.classList.add('animate__animated');
+      el5.classList.add('animate__fadeInDown');
+      el5.classList.add('block-visible');
+    }, 2000);
+    setTimeout(() => {
+      el6.classList.add('animate__animated');
+      el6.classList.add('animate__fadeInDown');
+      el6.classList.add('block-visible');
+    }, 2500);
+  });
+};
+animateBlockVantage(vantageWrapper);
+const apperanceWrapper = document.querySelector('.apperance');
+const animateBlockApperance = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el2 = document.querySelector('.apperance__img--first');
+    let el1 = document.querySelector('.apperance__img--second');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+  });
+};
+animateBlockApperance(apperanceWrapper);
+const paybackWrapper = document.querySelector('.payback');
+const animateBlockPayback = wrapper => {
+  wrapper.addEventListener('mouseover', () => {
+    let el1 = document.querySelector('.payback__item--first');
+    let el2 = document.querySelector('.payback__item--second');
+    let el3 = document.querySelector('.payback__item--three');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__zoomIn');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__zoomIn');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__zoomIn');
+      el3.classList.add('block-visible');
+    }, 1000);
+  });
+};
+animateBlockPayback(paybackWrapper);
 const ownWrapper = document.querySelector('.own');
-const animateBlock = (wrapper, block, animateClass) => {
+const animateBlockOwn = wrapper => {
   wrapper.addEventListener('mouseover', () => {
-    let el = document.querySelectorAll(block);
-    el.forEach(item => {
-      item.classList.add(animateClass);
-    });
+    let el1 = document.querySelector('.own__first--item__first');
+    let el2 = document.querySelector('.own__first--item__second');
+    let el3 = document.querySelector('.own__first--item__three');
+    setTimeout(() => {
+      el1.classList.add('animate__animated');
+      el1.classList.add('animate__fadeInLeft');
+      el1.classList.add('block-visible');
+    }, 0);
+    setTimeout(() => {
+      el2.classList.add('animate__animated');
+      el2.classList.add('animate__fadeInRight');
+      el2.classList.add('block-visible');
+    }, 500);
+    setTimeout(() => {
+      el3.classList.add('animate__animated');
+      el3.classList.add('animate__fadeInLeft');
+      el3.classList.add('block-visible');
+    }, 1000);
   });
-  // wrapper.addEventListener('mouseout', () => {
-  //   let el = document.querySelectorAll(block);
-  //   el.forEach(item => {
-  //     item.classList.remove(animateClass)
-  //   })
-  // })
 };
+animateBlockOwn(ownWrapper);
 
-animateBlock(aboutWrapper, '.about__block', 'animate__zoomIn');
-animateBlock(stagesWrapper, '.stages__item', 'animate__zoomIn');
-animateBlock(recastWrapper, '.recast__bg', 'animate__fadeInUp');
-animateBlock(ownWrapper, '.own__first--item__first', 'animate__bounceInLeft');
-animateBlock(ownWrapper, '.own__first--item__second', 'animate__bounceInRight');
-animateBlock(ownWrapper, '.own__first--item__three', 'animate__bounceInLeft');
-animateBlock(ownWrapper, '.own__second--item--first', 'animate__bounceInDown');
-animateBlock(ownWrapper, '.own__second--item--second', 'animate__bounceInUp');
-animateBlock(ownWrapper, '.own__second--item--three', 'animate__bounceInDown');
-const ownTitle = document.querySelectorAll('.own__second--item__title');
-const ownSubtitle = document.querySelectorAll('.own__second--item__text');
-const recastTitle = document.querySelectorAll('.recast__item--title');
-const recastSubtitle = document.querySelectorAll('.recast__item--descr');
-const animateText = (wrapper, title, subtitle) => {
-  wrapper.addEventListener('mouseover', () => {
-    title.forEach(item => {
-      item.classList.add('tracking-in-contract');
-    });
-    subtitle.forEach(item => {
-      item.classList.add('text-focus-in');
-    });
-  });
-  // wrapper.addEventListener('mouseout', () => {
-  //   contentTitle.forEach(item => {
-  //     item.classList.remove('tracking-in-contract')
-  //   })
-  //   contentDescr.forEach(item => {
-  //     item.classList.remove('text-focus-in')
-  //   })
-  // })
-};
-
-animateText(ownWrapper, ownTitle, ownSubtitle);
-animateText(recastWrapper, recastTitle, recastSubtitle);
 //  Animate
 
 const mySvg = new Vivus('mySvg', {
   type: 'sync',
-  duration: 200,
+  duration: 150,
   start: 'autostart'
 });
 const pdffull1 = new Vivus('pdffuel1', {
