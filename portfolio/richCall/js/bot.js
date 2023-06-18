2c042636739769bf74f939796d140f23813c8e1d
Isidas <99565745+Isidas@users.noreply.github.com>
2023-06-19 00:05:25 +0300

    Add files via upload

--- portfolio/richCall/js/bot.js
+++ portfolio/richCall/js/bot.js
@@ -1,2 +1,8 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
+document.addEventListener("DOMContentLoaded", () => {
+  let tg = window.Telegram.WebApp;
+  tg.expand();
+  const frame = document.querySelector("iframe");
+  frame.addEventListener("click", () => {
+    return frame;
+  });
+});
