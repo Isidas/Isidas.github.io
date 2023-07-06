3eb8fd53b0b649d6aa6e3ce50da2a9fdccd1c89b
Isidas <99565745+Isidas@users.noreply.github.com>
2023-07-06 22:56:20 +0300

    Delete portfolio/SubscriptionCentre directory

--- portfolio/SubscriptionCentre/js/script.js
+++
@@ -1,8 +0,0 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-window.addEventListener("load", function () {
-  window.location.href =
-    "https://cloud.mc.iqos.ru/subscription-center?personid=" +
-    $session.spice_id +
-    "&utm_medium=tg_bot&utm_campaign=subscription";
-});
