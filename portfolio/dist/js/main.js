534cfa7beffa92594cc599c01f82315725ecc231
Isidas <99565745+Isidas@users.noreply.github.com>
2023-07-12 00:05:12 +0300

    Add files via upload

--- portfolio/dist/js/main.js
+++ portfolio/dist/js/main.js
@@ -525,7 +525,7 @@ $(function () {
   function createTestList() {
     let text = "";
     testList.forEach((test) => {
-      text += `<a href='${test.link}' target="_blank" class="test__choose__item">${test.title}</a>`;
+      text += `<a href='${test.link}' target="_self" class="test__choose__item">${test.title}</a>`;
     });
     return text;
   }
