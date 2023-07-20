7e1b6678c50b3eb40af47794703005ead47020f3
Isidas <99565745+Isidas@users.noreply.github.com>
2023-07-20 20:21:56 +0300

    Add files via upload

--- portfolio/dist/js/main.js
+++ portfolio/dist/js/main.js
@@ -1,61 +1,64 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
+
 const botTitle = document.querySelector('.game_title');
-const itemWrapper = document.querySelector(".game_wrapper");
+const itemWrapper = document.querySelector('.game_wrapper');
 const container = document.querySelector('.container');
 const btnAgain = document.querySelector('.btn_again');
+const btnStart = document.querySelector('.btn_start');
+
 const pazzl = [
-  { id: 1, title: "Нет пепла", img: "1.png", right: true },
-  { id: 2, title: "Есть дым и пепел", img: "2.png", right: false },
+  { id: 1, title: 'Нет пепла', img: '1.png', right: true },
+  { id: 2, title: 'Есть дым и пепел', img: '2.png', right: false },
   {
     id: 3,
-    title: "Меньше едкого запаха,чем при курении сигарет",
-    img: "3.png",
+    title: 'Меньше едкого запаха,чем при курении сигарет',
+    img: '3.png',
     right: true,
   },
-  { id: 4, title: "Нет нагревания", img: "4.png", right: false },
-  { id: 5, title: "Настоящий вкус табака", img: "5.png", right: true },
-  { id: 6, title: "Нет сигаретного дыма", img: "6.png", right: true },
+  { id: 4, title: 'Нет нагревания', img: '4.png', right: false },
+  { id: 5, title: 'Настоящий вкус табака', img: '5.png', right: true },
+  { id: 6, title: 'Нет сигаретного дыма', img: '6.png', right: true },
 ];
 
 const wrongAnswer = [
   'Не совсем так',
   'Кажется, где-то ошибка',
-  'Чего-то не хватает'
+  'Чего-то не хватает',
 ];
 
 let gameValidateDiv;
 
-const createGameItem = () => {
+function createGameItem() {
   pazzl.forEach((item, index) => {
-    const gameItem = document.createElement("div");
-    gameItem.classList.add("game_item");
-    gameItem.dataset.id = item.id; // Устанавливаем атрибут data-id для каждого элемента
+    const gameItem = document.createElement('div');
+    gameItem.classList.add('game_item');
+    gameItem.dataset.id = item.id;
     gameItem.innerHTML = `
       <p class="item_title">${item.title}</p>
       <img src="./images/${item.img}" alt="pazzle" class="item_image item_image_${index}" />
     `;
 
-    gameItem.addEventListener("click", () => {
+    gameItem.addEventListener('click', () => {
       onGameItemClick(gameItem);
     });
 
     itemWrapper.appendChild(gameItem);
   });
-};
+}
 
 function addPuzzleField() {
-  const puzzleContainer = document.createElement("div");
-  puzzleContainer.classList.add("puzzle_container");
+  const puzzleContainer = document.createElement('div');
+  puzzleContainer.classList.add('puzzle_container');
 
   for (let i = 1; i <= 4; i++) {
-    const puzzleField = document.createElement("img");
-    puzzleField.classList.add("puzzle_field", `puzzle_field_${i}`);
-    puzzleField.src = "./images/7.png";
-    puzzleField.alt = "puzzle_wrapper";
+    const puzzleField = document.createElement('img');
+    puzzleField.classList.add('puzzle_field', `puzzle_field_${i}`);
+    puzzleField.src = './images/7.png';
+    puzzleField.alt = 'puzzle_wrapper';
     puzzleContainer.appendChild(puzzleField);
 
-    puzzleField.addEventListener("click", () => {
+    puzzleField.addEventListener('click', () => {
       clearPuzzleField(puzzleField);
     });
   }
@@ -64,26 +67,26 @@ function addPuzzleField() {
 }
 
 function clearPuzzleField(puzzleField) {
-  if (puzzleField.classList.contains("filled")) {
+  if (puzzleField.classList.contains('filled')) {
     const gameId = puzzleField.dataset.id;
     const gameItem = document.querySelector(`.game_item[data-id="${gameId}"]`);
-    puzzleField.src = "./images/7.png";
-    puzzleField.classList.remove("filled");
-    puzzleField.dataset.id = "";
-    gameItem.classList.remove("disabled");
+    puzzleField.src = './images/7.png';
+    puzzleField.classList.remove('filled');
+    puzzleField.dataset.id = '';
+    gameItem.classList.remove('disabled');
   }
 }
 
 function onGameItemClick(gameItem) {
-  if (gameItem.classList.contains("disabled")) return;
+  if (gameItem.classList.contains('disabled')) return;
 
-  const puzzleField = document.querySelector(".puzzle_container img:not(.filled)");
+  const puzzleField = document.querySelector('.puzzle_container img:not(.filled)');
   if (puzzleField) {
-    const itemImage = gameItem.querySelector("img");
+    const itemImage = gameItem.querySelector('img');
     puzzleField.src = itemImage.src;
-    puzzleField.dataset.id = gameItem.dataset.id; // Устанавливаем атрибут data-id для поля пазла
-    puzzleField.classList.add("filled");
-    gameItem.classList.add("disabled");
+    puzzleField.dataset.id = gameItem.dataset.id;
+    puzzleField.classList.add('filled');
+    gameItem.classList.add('disabled');
   }
 }
 
@@ -93,17 +96,17 @@ function getRandomWrongAnswer() {
 }
 
 window.onload = () => {
-  gameValidateDiv = document.querySelector(".game_validate");
+  gameValidateDiv = document.querySelector('.game_validate');
   if (!gameValidateDiv) {
     console.error("Элемент с классом 'game_validate' не найден на странице.");
     return;
   }
 
-  const checkButton = document.createElement("button");
+  const checkButton = document.createElement('button');
   checkButton.classList.add('btn_validate', 'btn_game');
-  checkButton.textContent = "Готово";
-  checkButton.addEventListener("click", () => {
-    const puzzleFields = document.querySelectorAll(".puzzle_field");
+  checkButton.textContent = 'Готово';
+  checkButton.addEventListener('click', () => {
+    const puzzleFields = document.querySelectorAll('.puzzle_field');
     const isPuzzleCorrect = Array.from(puzzleFields).every((field) => {
       const fieldId = field.dataset.id;
       const puzzle = pazzl.find((item) => item.id === Number(fieldId));
@@ -114,16 +117,17 @@ window.onload = () => {
     if (isPuzzleCorrect) {
       checkButton.classList.add('btn_hide');
       btnAgain.classList.remove('btn_hide');
-      itemWrapper.style.display = "none";
-      botTitle.innerHTML = '<b>Прекрасно! Вы отлично справились!</b>Главное преимущество — отсутствие горения.А значит';
+      itemWrapper.style.display = 'none';
+      botTitle.innerHTML =
+        '<b>Прекрасно! Вы отлично справились!</b>Главное преимущество — отсутствие горения.А значит';
     } else {
       checkButton.classList.add('btn_hide');
       btnAgain.classList.remove('btn_hide');
-      gameValidateDiv.style.display = "none";
-      itemWrapper.style.display = "none";
-      botTitle.style.display = "none";
-      const wrongFrame = document.createElement("div");
-      wrongFrame.classList.add("quize__x__item", "quize__x__item--status");
+      gameValidateDiv.style.display = 'none';
+      itemWrapper.style.display = 'none';
+      botTitle.style.display = 'none';
+      const wrongFrame = document.createElement('div');
+      wrongFrame.classList.add('quize__x__item', 'quize__x__item--status');
       wrongFrame.innerHTML = `
         <div class='quize__x__item--status__info'>
           <div class='quize__x__item--status__info__inner'>
@@ -140,6 +144,12 @@ window.onload = () => {
 
   btnAgain.addEventListener('click', () => {
     location.reload();
+  });
+
+  btnStart.addEventListener('click', () => {
+    itemWrapper.classList.remove('hide');
+    gameValidateDiv.classList.remove('hide');
+    btnStart.classList.add('hide');
   });
 
   gameValidateDiv.appendChild(checkButton);
