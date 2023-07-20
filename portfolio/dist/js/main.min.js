6ecf95f5df3d466ff5f7a2f0a01fafaf09a731dc
Isidas <99565745+Isidas@users.noreply.github.com>
2023-07-20 03:20:53 +0300

    Add files via upload

--- portfolio/dist/js/main.min.js
+++ portfolio/dist/js/main.min.js
@@ -1,4 +1,4 @@
-let tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 tg.expand();
 const pazzl = [
   { id: 1, title: "Нет пепла", img: "1.png", right: true },
@@ -19,25 +19,14 @@ const createGameItem = () => {
   pazzl.forEach((item, index) => {
     const gameItem = document.createElement("div");
     gameItem.classList.add("game_item");
+    gameItem.dataset.id = item.id; // Устанавливаем атрибут data-id для каждого элемента
     gameItem.innerHTML = `
       <p class="item_title">${item.title}</p>
       <img src="./images/${item.img}" alt="pazzle" class="item_image item_image_${index}" />
     `;
 
-    // Добавляем обработчик события при клике на элемент
     gameItem.addEventListener("click", () => {
-      // Делаем элемент серым и неактивным
-      gameItem.classList.add("grayed-out");
-      gameItem.classList.add("disabled");
-
-      // Находим первое свободное поле пазла
-      const puzzleField = document.querySelector(".puzzle_container img:not(.filled)");
-      if (puzzleField) {
-        // Помещаем картинку элемента в поле пазла
-        const itemImage = gameItem.querySelector("img");
-        puzzleField.src = itemImage.src;
-        puzzleField.classList.add("filled");
-      }
+      onGameItemClick(gameItem);
     });
 
     itemWrapper.appendChild(gameItem);
@@ -47,11 +36,9 @@ const createGameItem = () => {
 function addPuzzleField() {
   const gameValidateDiv = document.querySelector(".game_validate");
 
-  // Создание контейнера для полей пазла
   const puzzleContainer = document.createElement("div");
   puzzleContainer.classList.add("puzzle_container");
 
-  // Создание и добавление полей пазла
   for (let i = 1; i <= 4; i++) {
     const puzzleField = document.createElement("img");
     puzzleField.classList.add("puzzle_field", `puzzle_field_${i}`);
@@ -60,32 +47,43 @@ function addPuzzleField() {
     puzzleContainer.appendChild(puzzleField);
   }
 
-  // Добавление контейнера с полями пазла в game_validateDiv
   gameValidateDiv.appendChild(puzzleContainer);
 }
 
-// Вызов функции для добавления поля пазла
-addPuzzleField();
-createGameItem();
+function onGameItemClick(gameItem) {
+  if (gameItem.classList.contains("disabled")) return; // Если элемент уже использован, ничего не делаем
 
-// Добавление кнопки для проверки правильности сборки пазлов
-const checkButton = document.createElement("button");
-checkButton.textContent = "Проверить";
-checkButton.addEventListener("click", () => {
-  const puzzleFields = document.querySelectorAll(".puzzle_field");
-  const isPuzzleCorrect = Array.from(puzzleFields).every((field, index) => {
-    // Проверяем соответствие правильности сборки
-    const puzzleIndex = index + 1;
-    const puzzle = pazzl.find((item) => item.id === puzzleIndex);
-    return field.src.includes(puzzle.img) === puzzle.right;
+  const puzzleField = document.querySelector(".puzzle_container img:not(.filled)");
+  if (puzzleField) {
+    const itemImage = gameItem.querySelector("img");
+    puzzleField.src = itemImage.src;
+    puzzleField.dataset.id = gameItem.dataset.id; // Устанавливаем атрибут data-id для поля пазла
+    puzzleField.classList.add("filled");
+    gameItem.classList.add("disabled");
+  }
+}
+
+window.onload = () => {
+  addPuzzleField();
+  createGameItem();
+
+  const checkButton = document.createElement("button");
+  checkButton.textContent = "Проверить";
+  checkButton.addEventListener("click", () => {
+    const puzzleFields = document.querySelectorAll(".puzzle_field");
+    const isPuzzleCorrect = Array.from(puzzleFields).every((field) => {
+      const fieldId = field.dataset.id;
+      const puzzle = pazzl.find((item) => item.id === Number(fieldId));
+      return field.src.includes(puzzle.img) === puzzle.right;
+    });
+
+    if (isPuzzleCorrect) {
+      alert("Пазлы собраны верно!");
+    } else {
+      alert("Пазлы собраны неверно!");
+    }
   });
 
-  if (isPuzzleCorrect) {
-    alert("Пазлы собраны верно!");
-  } else {
-    alert("Пазлы собраны неверно!");
-  }
-});
-
-const gameValidateDiv = document.querySelector(".game_validate");
-gameValidateDiv.appendChild(checkButton);
+  const gameValidateDiv = document.querySelector(".game_validate");
+  gameValidateDiv.appendChild(checkButton);
+};
